e9695a7e4d495c71517b961a2ac2576d7f572bcf
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2025-05-20 01:30:22 +0530

    footer

--- data/index.ts
+++ data/index.ts
@@ -243,7 +243,7 @@ export const projects2 = [
     iconLists: ["/js.svg", "/html.svg", "/bootstrap.svg", "/auth.svg"],
     link: "https://github.com/amankushwaha577/Friend-s-Spy",
   },
-  { 
+  {
     id: 2,
     title: "Devfolio",
     des: "Step into our store and find your happy place.",
@@ -251,14 +251,15 @@ export const projects2 = [
     iconLists: ["/js.svg", "/html.svg", "/bootstrap.svg", "/c.svg"],
     link: "/ui.yoom.com",
   },
- {
+  {
     id: 3,
     title: "Portfolio",
     des: "Step into our store and find your happy place.",
     img: "/portfolio.png",
     iconLists: ["/js.svg", "/html.svg", "/bootstrap.svg", "/c.svg"],
     link: "/ui.yoom.com",
-  },];
+  },
+];
 export const projects1 = [
   {
     id: 1,
@@ -347,31 +348,31 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
+    title: "SDE-2 @SAMSUNG INDIA",
     desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
+    title: "Frontend Developer @Ingenuity",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 3,
+    title: "30+ Industrial Project Support",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    id: 4,
+    title: "2x GATE Qualified",
+    desc: "Two time GATE qualified in 2021 & 2022 Respectively in Computer Science.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
   },
 ];
 
